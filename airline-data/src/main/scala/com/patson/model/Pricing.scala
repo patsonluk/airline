dd3a1a1b9ee02e4b5882e448d726c66b6a45479d
patsonluk <patson_luk@hotmail.com>
2016-01-04 23:17:03 -0800

    adjustment to pricing. Very short route should be more expansive

--- airline-data/src/main/scala/com/patson/model/Pricing.scala
+++ airline-data/src/main/scala/com/patson/model/Pricing.scala
@@ -7,18 +7,19 @@ import FlightType._
 object Pricing {
   val INTERNATIONAL_PRICE_MULTIPLIER = 1.25
   
-  //200 km = 150
+  //base 100
+  //200 km = 100 + 50
   //1000 km = 150 + 100 = 250  (800 * 0.125) 
   //2000 km = 250 + 100 = 350  (1000 * 0.1)
   //10000 km = 350 + 400 = 750 (8000 * 0.05)
-  val modifierBrackets = List((200, 0.75),(800, 0.125),(1000, 0.1),(Int.MaxValue, 0.05))
+  val modifierBrackets = List((200, 0.25),(800, 0.125),(1000, 0.1),(Int.MaxValue, 0.05))
   
   def computeStandardPrice(link : Link, linkClass : LinkClass) : Int = {
     computeStandardPrice(link.distance, Computation.getFlightType(link.from, link.to), linkClass)
   }
   def computeStandardPrice(distance : Int, flightType : FlightType, linkClass : LinkClass) : Int = {
     var remainDistance = distance
-    var price = 0.0
+    var price = 100.0
     for (priceBracket <- modifierBrackets if(remainDistance > 0)) {
       if (priceBracket._1 >= remainDistance) {
         price += remainDistance * priceBracket._2
