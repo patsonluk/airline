1e100275039ffc07431f24eabcc0fc1023573849
patsonluk <patson@solarwinds.cloud>
2018-05-18 22:48:52 -0700

    Fine tuning on quality adjustment calculation

--- airline-data/src/main/scala/com/patson/AirlineSimulation.scala
+++ airline-data/src/main/scala/com/patson/AirlineSimulation.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration.Duration
 object AirlineSimulation {
   private val AIRLINE_FIXED_COST = 0 //for now...
   private val REPUTATION_INCREMENT = 0.5 
-  private val SERVICE_DELTA = 1
+  private[patson] val MAX_SERVICE_QUALITY_INCREMENT : Double = 1
   
   def airlineSimulation(cycle: Int, linkResult : List[LinkConsumptionDetails]) = {
     //compute profit
@@ -49,26 +49,12 @@ object AirlineSimulation {
           
            val totalCapacity = links.map { _.capacity.total }.sum
            if (totalCapacity > 0) {
-             var targetServiceQuality = airline.getServiceFunding() * 5 / totalCapacity
-             if (targetServiceQuality > 100) {
-               targetServiceQuality = 100
-             }
+             val targetServiceQuality = getTargetQuality(airline.getServiceFunding(), totalCapacity) //50x to get 50 target quality, 200x to get max 100 target quality
              val currentServiceQuality = airline.getServiceQuality()
-             if (currentServiceQuality < targetServiceQuality) {
-                if (currentServiceQuality + SERVICE_DELTA >= targetServiceQuality) {
-                  airline.setServiceQuality(targetServiceQuality)  
-                } else {
-                  airline.setServiceQuality(currentServiceQuality + SERVICE_DELTA)
-                } 
-              } else {
-                if (currentServiceQuality - SERVICE_DELTA <= targetServiceQuality) {
-                  airline.setServiceQuality(targetServiceQuality) 
-                } else {
-                  airline.setServiceQuality(currentServiceQuality - SERVICE_DELTA)
-                }
-              }
+             airline.setServiceQuality(getNewQuality(currentServiceQuality, targetServiceQuality)) 
            } 
         }
+        
         airlineProfit -= airline.getServiceFunding()
         
         airlineProfit -= AIRLINE_FIXED_COST
@@ -78,5 +64,36 @@ object AirlineSimulation {
     }
     
     AirlineSource.saveAirlineInfo(allAirlines)
+  }
+  
+  val getTargetQuality : (Int, Int) => Double = (funding : Int, capacity :Int) => {
+    val computedQuality = Math.sqrt(funding.toDouble / capacity / 50 ) * 50  //50x capacity to get 50 target quality, 200x capacity to get max 100 target quality
+    if (computedQuality >= Airline.MAX_SERVICE_QUALITY) {
+      Airline.MAX_MAINTENANCE_QUALITY
+    } else {
+      computedQuality
+    }
+  }
+  val getNewQuality : (Double, Double) => Double = (currentQuality, targetQuality) =>  {
+    val delta = targetQuality - currentQuality
+    val adjustment = 
+      if (delta >= 0) { //going up, slower when current quality is already high
+        MAX_SERVICE_QUALITY_INCREMENT * (1 - (currentQuality / Airline.MAX_SERVICE_QUALITY * 0.9)) //at current quality 0, multiplier 1x; current quality 100, multiplier 0.1x
+      } else { //going down, faster when current quality is already high
+        -1 * MAX_SERVICE_QUALITY_INCREMENT * (0.1 + (currentQuality / Airline.MAX_SERVICE_QUALITY * 0.9)) //at current quality 0, multiplier 0.1x; current quality 100, multiplier 1x
+      }
+    if (adjustment >= 0) {
+      if (adjustment + currentQuality >= targetQuality) {
+        targetQuality
+      } else {
+        adjustment + currentQuality
+      }
+    } else {
+      if (currentQuality + adjustment <= targetQuality) {
+        targetQuality
+      } else {
+        currentQuality + adjustment
+      }
+    } 
   }
 }
---
+++ airline-data/src/test/scala/com/patson/AirlineSimulationSpec.scala
@@ -0,0 +1,46 @@
+package com.patson
+
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.Matchers
+import org.scalatest.WordSpecLike
+import akka.actor.ActorSystem
+import akka.testkit.ImplicitSender
+import akka.testkit.TestKit
+import com.patson.model._
+import scala.collection.mutable.Set
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+import com.patson.model.airplane._
+ 
+class AirlineSimulationSpec extends WordSpecLike with Matchers {
+  "getTargetQuality".must {
+    "50x capacity to get 50 target quality, 200x capacity to get max 100 target quality".in {
+       assert(AirlineSimulation.getTargetQuality(0, 1000) == 0) //X0 capacity funding
+       assert(AirlineSimulation.getTargetQuality(5000, 1000) > 0) //X5 capacity funding
+       assert(AirlineSimulation.getTargetQuality(50000, 1000) == 50) //X50 capacity funding
+       assert(AirlineSimulation.getTargetQuality(200000, 1000) == 100) //X200 capacity funding
+       assert(AirlineSimulation.getTargetQuality(300000, 1000) == 100) //X300 hit max
+    }
+  }
+  "getNewQuality".must {
+    "for increment at current quality 0, multiplier 1x; current quality 100, multiplier 0.1x".in {
+       assert(AirlineSimulation.getNewQuality(0, Airline.MAX_SERVICE_QUALITY) == 0 + AirlineSimulation.MAX_SERVICE_QUALITY_INCREMENT) //get full increment
+       assert(AirlineSimulation.getNewQuality(Airline.MAX_SERVICE_QUALITY, Airline.MAX_SERVICE_QUALITY) == Airline.MAX_SERVICE_QUALITY) //no increment
+       assert(AirlineSimulation.getNewQuality(50, Airline.MAX_SERVICE_QUALITY) < 50 + AirlineSimulation.MAX_SERVICE_QUALITY_INCREMENT) //slow down
+       assert(AirlineSimulation.getNewQuality(50, Airline.MAX_SERVICE_QUALITY) > 50) //but should still have increment
+       assert(AirlineSimulation.getNewQuality(Airline.MAX_SERVICE_QUALITY - 1, Airline.MAX_SERVICE_QUALITY) < Airline.MAX_SERVICE_QUALITY) //slow down
+       assert(AirlineSimulation.getNewQuality(Airline.MAX_SERVICE_QUALITY - 1, Airline.MAX_SERVICE_QUALITY) > Airline.MAX_SERVICE_QUALITY - 1) //but should still have increment
+    }
+    "for decrement at current quality 0, multiplier 0.1x; current quality 100, multiplier 1x".in {
+       assert(AirlineSimulation.getNewQuality(Airline.MAX_SERVICE_QUALITY, 0) == Airline.MAX_SERVICE_QUALITY - AirlineSimulation.MAX_SERVICE_QUALITY_INCREMENT) //get full decrement
+       assert(AirlineSimulation.getNewQuality(0, 0) == 0) //no decrement
+       assert(AirlineSimulation.getNewQuality(50, 0) > 50 - AirlineSimulation.MAX_SERVICE_QUALITY_INCREMENT) //slow down
+       assert(AirlineSimulation.getNewQuality(50, 0) < 50) //but should still have decrement
+       assert(AirlineSimulation.getNewQuality(1, 0) > 0) //slow down
+       assert(AirlineSimulation.getNewQuality(1, 0) < 1) //but should still have decrement
+    }
+  }
+  
+ 
+}
