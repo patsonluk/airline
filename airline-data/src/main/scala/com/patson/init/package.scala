ed1abb0ecc841b6235ae3a94a76af0331067f0e1
patsonluk <patson@solarwinds.cloud>
2018-05-19 19:11:06 -0700

    Changed codec to always use UTF-8

--- airline-data/src/main/scala/com/patson/init/package.scala
+++ airline-data/src/main/scala/com/patson/init/package.scala
@@ -2,7 +2,10 @@ package com.patson
 
 import akka.actor.ActorSystem
 import akka.stream.FlowMaterializer
+import scala.io.Codec
+
 package object init {
   implicit val actorSystem = ActorSystem("init-stream")
   implicit val materializer = FlowMaterializer()
+  implicit val codec = Codec.UTF8
 }
