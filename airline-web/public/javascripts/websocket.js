cc82643f3849698f634c890804673597da9cf06f
patsonluk <patson@solarwinds.cloud>
2018-06-04 23:40:39 -0700

    Fixed websocket port

--- airline-web/public/javascripts/websocket.js
+++ airline-web/public/javascripts/websocket.js
@@ -1,4 +1,4 @@
-var wsUri = "ws://" +  window.location.hostname + ":9000/wsWithActor";
+var wsUri = "ws://" +  window.location.hostname + ":" + window.location.port + "/wsWithActor";
 var websocket;
 var selectedAirlineId
 
