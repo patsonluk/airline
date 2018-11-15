440712f568086013715a7267cffb143b79c2d6b5
daprof1 <dr.mjinman@gmail.com>
2018-11-14 21:15:44 -0600

    Update airline-web/app/controllers/ClientActor.scala
    Little bit of chat filtering for jerks who want to ruin things

--- airline-web/app/controllers/ClientActor.scala
+++ airline-web/app/controllers/ClientActor.scala
@@ -6,7 +6,7 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 class ClientActor(out: ActorRef, chat: ActorRef) extends Actor {
-
+  val bannedwords = List("anal","anus","arse","ass","ballsack","balls","bastard","bitch","biatch","bloody","blowjob","blow job","bollock","bollok","boner","boob","bugger","bum","butt","buttplug","clitoris","cock","coon","crap","cunt","damn","dick","dildo","dyke","fag","feck","fellate","fellatio","felching","fuck","f u c k","fudgepacker","fudge packer","flange","Goddamn","God damn","hell","homo","jerk","jizz","knobend","knob end","labia","lmao","lmfao","muff","nigger","nigga","omg","penis","piss","poop","prick","pube","pussy","queer","scrotum","sex","shit","s hit","sh1t","slut","smegma","spunk","tit","tosser","turd","twat","vagina","wank","whore","wtf")
   chat ! Join
 
   override def postStop() = chat ! Leave
@@ -14,13 +14,14 @@ class ClientActor(out: ActorRef, chat: ActorRef) extends Actor {
   def receive = {
     // this handles messages from the websocket
     case text: String =>
-	  val sdf = new SimpleDateFormat("HH:mm:ss")
-      if (text.indexOf("[LOGGED]") > -1) {
-	   chat ! ClientSentMessage(text)
-	  } else {
-	  chat ! ClientSentMessage("[" + sdf.format(Calendar.getInstance().getTime())+ "] " + text)
-	  }
-	   
+		if (!bannedwords.exists(text.contains)) {
+		  val sdf = new SimpleDateFormat("HH:mm:ss")
+		  if (text.indexOf("[LOGGED]") > -1) {
+		   chat ! ClientSentMessage(text)
+		  } else {
+		  chat ! ClientSentMessage("[" + sdf.format(Calendar.getInstance().getTime())+ "] " + text)
+		  }
+		}   
     case ClientSentMessage(text) =>
       out ! text.replaceFirst("\\W*(\\[LOGGED\\])","")
   }
