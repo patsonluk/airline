b9e8bf329c983cfd3c888a26635c671e1e8f4f58
patsonluk <patson@solarwinds.cloud>
2018-12-23 17:21:48 -0800

    https://github.com/patsonluk/airline/issues/139

--- airline-data/src/main/scala/com/patson/init/AirportSizeAdjust.scala
+++ airline-data/src/main/scala/com/patson/init/AirportSizeAdjust.scala
@@ -240,6 +240,7 @@ object AirportSizeAdjust {
         "YLW" -> 3, //Kelowna
         "MDE" -> 5, //https://en.wikipedia.org/wiki/Jos%C3%A9_Mar%C3%ADa_C%C3%B3rdova_International_Airport around 7 mil passengers
         "BDA" -> 5, // Bermuda
+        "DUB" -> 5, // Dublin
         "BHX" -> 5 //https://en.wikipedia.org/wiki/Birmingham_Airport around 13 mil passengers, A380 capable
       )
       
