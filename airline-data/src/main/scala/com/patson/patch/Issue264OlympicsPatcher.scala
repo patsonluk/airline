640666f8f90ebbdc7b771b3a5d8f2317a354f3e6
patsonluk <patson@solarwinds.cloud>
2020-02-05 22:16:59 -0800

    Patcher for Olympics changes

---
+++ airline-data/src/main/scala/com/patson/patch/Issue264OlympicsPatcher.scala
@@ -0,0 +1,29 @@
+package com.patson.patch
+
+import java.sql.Connection
+
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import com.patson.data.Constants.{DATABASE_CONNECTION, DATABASE_PASSWORD, DATABASE_USER, DB_DRIVER}
+import com.patson.data.Meta
+
+/**
+  * patcher for v1.1
+  */
+object Issue264OlympicsPatcher extends App {
+  Class.forName(DB_DRIVER)
+  val dataSource = new ComboPooledDataSource()
+  dataSource.setUser(DATABASE_USER)
+  dataSource.setPassword(DATABASE_PASSWORD)
+  dataSource.setJdbcUrl(DATABASE_CONNECTION)
+  dataSource.setMaxPoolSize(100)
+
+  def createSchema(connection: Connection) = {
+    val connection = dataSource.getConnection()
+    try {
+      Meta.createEvent(connection)
+      Meta.createAirportAirlineBonus(connection)
+    } finally {
+      connection.close
+    }
+  }
+}
