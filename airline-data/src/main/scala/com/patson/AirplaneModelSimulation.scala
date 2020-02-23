6819de179adf18082eb70895d48dc0bd92de060c
patsonluk <patson@solarwinds.cloud>
2020-02-23 12:20:27 -0800

    Fixed discount when there's no airplane in circulation

--- airline-data/src/main/scala/com/patson/AirplaneModelSimulation.scala
+++ airline-data/src/main/scala/com/patson/AirplaneModelSimulation.scala
@@ -26,10 +26,17 @@ object AirplaneModelSimulation {
     //purge all the existing discounts due to low demand
     val airplanesByModel = allAirplanes.groupBy(_.model)
     val allModelDiscounts = ListBuffer[ModelDiscount]()
-    airplanesByModel.foreach {
-      case (model, airplanes) =>
-        allModelDiscounts.appendAll(getModelDiscountsByAirplaneCount(model, airplanes.length))
+//    airplanesByModel.foreach {
+//      case (model, airplanes) =>
+//        allModelDiscounts.appendAll(getModelDiscountsByAirplaneCount(model, airplanes.length))
+//    }
+    ModelSource.loadAllModels().foreach { model =>
+      allModelDiscounts.appendAll(getModelDiscountsByAirplaneCount(model, airplanesByModel.get(model) match {
+        case Some(airplanes) => airplanes.length
+        case None => 0
+      }))
     }
+
 
     ModelSource.updateModelDiscounts(allModelDiscounts.toList)
   }
