5b4501c77659e2cc9e10013710c9a2cc0d0b88fc
patsonluk <patson@solarwinds.cloud>
2020-12-10 22:36:16 -0800

    Fixed delegate weeks remaining

--- airline-data/src/main/scala/com/patson/MainSimulation.scala
+++ airline-data/src/main/scala/com/patson/MainSimulation.scala
@@ -71,9 +71,6 @@ object MainSimulation extends App {
       println("Purging link history")
       ChangeHistorySource.deleteLinkChangeByCriteria(List(("cycle", "<", cycle - 100)))
 
-      //refresh delegates
-      DelegateSource.deleteBusyDelegateByCriteria(List(("available_cycle", "<=", cycle)))
-      
       //notify the websockets via EventStream
       println("Publish Cycle Complete message")
       SimulationEventStream.publish(CycleCompleted(cycle), None)
@@ -82,29 +79,34 @@ object MainSimulation extends App {
       println("cycle " + cycle + " spent " + (cycleEnd - cycleStartTime) / 1000 + " secs")
   }
 
-  def postCycle(simulatedCycle : Int) = {
+  def postCycle(currentCycle : Int) = {
     //now update the link capacity if necessary
     LinkSimulation.refreshLinksPostCycle()
     println("Oil simulation")
-    OilSimulation.simulate(simulatedCycle + 1) //simulate it for +1 as if the simulation is fast, it will sit at a new week until next start cycle
+    OilSimulation.simulate(currentCycle) //simulate at the beginning of a new cycle
     println("Loan simulation")
-    LoanInterestRateSimulation.simulate(simulatedCycle + 1) //simulate it for +1 as if the simulation is fast, it will sit at a new week until next start cycle
-
+    LoanInterestRateSimulation.simulate(currentCycle) //simulate at the beginning of a new cycle
+    //refresh delegates
+    println("Refresh delegates")
+    DelegateSource.deleteBusyDelegateByCriteria(List(("available_cycle", "<=", currentCycle)))
   }
-  
-  
-  
- 
-  
+
+
+  /**
+    * The simulation can be seen like this:
+    * On week(cycle) n. It starts the long simulation (pax simulation) at the "END of the week"
+    * when it finishes computing the pax of the past week. It sets the current week to next week (which indicates a beginning of week n + 1)
+    * It then runs some postCycle task (these tasks should be short and can be regarded as things to do at the Beginning of a week)
+    *
+    */
   class MainSimulationActor extends Actor {
     currentWeek = CycleSource.loadCycle()
     def receive = {
       case Start =>
-        val simulationCycle = currentWeek
-        startCycle(simulationCycle)
+        startCycle(currentWeek)
         currentWeek += 1
         CycleSource.setCycle(currentWeek)
-        postCycle(simulationCycle)
+        postCycle(currentWeek) //post cycle do some quick updates, no long simulation
     }
   }
    
