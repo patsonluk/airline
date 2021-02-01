365ef28d73f7073f94a2eb55db0d3a089714446d
patsonluk <patson@solarwinds.cloud>
2021-01-31 21:48:31 -0800

    Improvement to airport animation lookup logic: 1. Don't throw error if there's no video 2. Find nearby non airport animation (by city served)

--- airline-web/app/controllers/AirportAnimationUtil.scala
+++ airline-web/app/controllers/AirportAnimationUtil.scala
@@ -9,11 +9,16 @@ import scala.util.Random
 object AirportAnimationUtil {
   val random = new Random()
   val sampleAnimationsByAirportSize = AirportAnimationSource.loadAirportAnimationByType(AirportAnimationType.AIRPORT).groupBy(_.airport.size)
-  val defaultAnimation = sampleAnimationsByAirportSize.toList.sortBy(_._1).head._2.head //smallest airport
+  val nearbyAirports : Map[(String, String), List[Airport]] = findNearbyAirports()
+
+  val defaultAnimation = sampleAnimationsByAirportSize.toList.sortBy(_._1).headOption match {
+    case Some((size, animationsOfSmallestAirport)) => animationsOfSmallestAirport.head
+    case None => AirportAnimation(Airport.fromId(0), AirportAnimationType.AIRPORT, "") //dummy
+  } //smallest airport
 
   def getAnimation(candidates : Airport*) : AirportAnimation = {
     candidates.foreach { candidate =>
-      val animations = AirportAnimationSource.loadAirportAnimationByAirportId(candidate.id)
+      val animations = AirportAnimationSource.loadAirportAnimationByAirportId(candidate.id) ++ nearByAnimations(candidate)
       if (!animations.isEmpty) {
         return animations(random.nextInt(animations.size))
       }
@@ -27,6 +32,23 @@ object AirportAnimationUtil {
     }
 
     return defaultAnimation
+  }
+
+  def nearByAnimations(airport : Airport) = {
+    nearbyAirports.get((airport.city, airport.countryCode)) match {
+      case Some(candidates) => candidates.filter(candidate => candidate.id != airport.id).flatMap { nearbyAirport =>
+        AirportAnimationSource.loadAirportAnimationByAirportId(nearbyAirport.id).filter(_.animationType != AirportAnimationType.AIRPORT) //only get non airport animations
+      }
+      case None => List.empty
+    }
+  }
+
+  /**
+   * find Airports that serve the same city
+   */
+  def findNearbyAirports() = {
+    val airportsByCity = cachedAirportsByPower.filterNot(_.city.isBlank).groupBy(airport => (airport.city, airport.countryCode)) //might need to split since there are cities with same name within the country. o well...
+    airportsByCity.filter(_._2.size <= 1) //filter out loners
   }
 }
 
