c256d2a02030f9fe470ab7edea29aeebf104bce7
patsonluk <patson@solarwinds.cloud>
2021-02-21 22:04:39 -0800

    Country relationship patch. With EU

--- airline-data/src/main/scala/com/patson/init/CountryMutualRelationshipGenerator.scala
+++ airline-data/src/main/scala/com/patson/init/CountryMutualRelationshipGenerator.scala
@@ -1,34 +1,73 @@
 package com.patson.init
 
 import com.patson.data.CountrySource
+
+import scala.collection.mutable
 import scala.collection.mutable.Map
 
 object CountryMutualRelationshipGenerator extends App {
+  val AFFILIATIONS = List(
+    Affiliation("EU", 2, List(
+      "BE", "GR", "LT", "PT", "BG", "ES", "LU", "RO", "CZ", "FR", "HU", "SI", "DK", "HR", "MT", "SK", "DE", "IT", "NL", "FI", "EE", "CY", "AT", "SE", "IE", "LV", "PL"
+    ))
+  )
+
+
   mainFlow()
-  
+
+
+
   def mainFlow() = {
-    val mutualRelationshipMap = getCountryMutalRelationship()
-    val mutualRelationshipPatchMap = getCountryMutalRelationshipPatch()
-    
-    val finalMutalRelationshipMap = mutualRelationshipMap ++ mutualRelationshipPatchMap
-    
-    println("Saving country mutual relationships: " + finalMutalRelationshipMap)
-    
-    CountrySource.updateCountryMutualRelationships(finalMutalRelationshipMap)
+    val mutualRelationshipMap = getCountryMutualRelationship()
+    val mutualRelationshipPatchMap = getCountryMutualRelationshipPatch()
+
+
+    val finalMutualRelationshipMap = affiliationAdjustment(mutualRelationshipMap ++ mutualRelationshipPatchMap)
+
+    println("Saving country mutual relationships: " + finalMutualRelationshipMap)
+
+    CountrySource.updateCountryMutualRelationships(finalMutualRelationshipMap)
   }
-  
+
+  def affiliationAdjustment(existingMap : mutable.Map[(String, String), Int]) : Map[(String, String), Int] = {
+    AFFILIATIONS.foreach {
+      case Affiliation(id, relationship, members) =>
+        members.foreach { memberX =>
+          if (CountrySource.loadCountryByCode(memberX).isDefined) {
+            members.foreach { memberY =>
+              if (memberX != memberY) {
+                val shouldPatch = existingMap.get((memberX, memberY)) match {
+                  case Some(existingValue) => existingValue < relationship
+                  case None => true
+                }
+                if (shouldPatch) {
+                  println(s"patching $memberX vs $memberY from $id with $relationship")
+                  existingMap.put((memberX, memberY), relationship)
+                } else {
+                  println(s"Not patching $memberX vs $memberY from $id with $relationship as existing value is greater")
+                }
+              }
+            }
+          } else {
+            println(s"Country code $memberX not found")
+          }
+        }
+    }
+    existingMap
+  }
+
   /**
    * get from country-mutual-relationship.csv
    */
-  def getCountryMutalRelationship() = {
+  def getCountryMutualRelationship() = {
     val nameToCode = CountrySource.loadAllCountries().map( country => (country.name, country.countryCode)).toMap
     val linesIter = scala.io.Source.fromFile("country-mutual-relationship.csv").getLines()
     val headerLine = linesIter.next()
-    
+
     val countryHeader = headerLine.split(',').filter(!_.isEmpty())
-    
+
     val mutualRelationshipMap = Map[(String, String), Int]()
-    
+
     while (linesIter.hasNext) {
       val tokens = linesIter.next().split(',').filter(!_.isEmpty())
       //first token is the country name itself
@@ -39,24 +78,24 @@ object CountryMutualRelationshipGenerator extends App {
         val toCountry = countryHeader(i - 1)
         //println(fromCountry + " " + toCountry + " " + strength)
         if (strength > 0) {
-          if (nameToCode.contains(fromCountry) && nameToCode.contains(toCountry)) { 
+          if (nameToCode.contains(fromCountry) && nameToCode.contains(toCountry)) {
             mutualRelationshipMap.put((nameToCode(fromCountry), nameToCode(toCountry)), strength)
           }
         }
       }
     }
-    
+
     nameToCode.values.foreach { countryCode =>
       mutualRelationshipMap.put((countryCode, countryCode), 5) //country with itself is 5 HomeCountry
     }
-    
+
     mutualRelationshipMap
   }
 
   /**
    * patch from country-mutual-relationship-patch.csv
    */
-  def getCountryMutalRelationshipPatch() = {
+  def getCountryMutualRelationshipPatch() = {
     val linesIter = scala.io.Source.fromFile("country-mutual-relationship-patch.csv").getLines()
     val mutualRelationshipMap = Map[(String, String), Int]()
     
@@ -71,6 +110,9 @@ object CountryMutualRelationshipGenerator extends App {
     }
     mutualRelationshipMap
   }
-  
-  
+
+  case class Affiliation(id : String, relationship: Int, members : List[String])
+
+
+
 }
