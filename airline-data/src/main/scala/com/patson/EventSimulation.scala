3c5fd63b88b63b0f72bf00afc0d82a06ba3b1282
patsonluk <patson@solarwinds.cloud>
2021-03-21 22:05:01 -0700

    Added olympics zone cooldown

--- airline-data/src/main/scala/com/patson/EventSimulation.scala
+++ airline-data/src/main/scala/com/patson/EventSimulation.scala
@@ -97,7 +97,7 @@ object EventSimulation {
   }
 
   val HOST_COUNTRY_COOLDOWN = 4 //should not be hosting it in the last 4 times
-  val HOST_ZONE_COOLDOWN = 2 //should not be hosting it in last 2 times
+  val HOST_ZONE_COOLDOWN = 1 //should not be hosting it in last time
   def selectCandidates(allAirports : List[Airport]) : List[Airport] = {
     val previousOlympics = EventSource.loadEvents().filter(_.eventType == EventType.OLYMPICS).sortBy(_.startCycle)
 
