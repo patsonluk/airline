135b2a2807c1950fc2a35b2a8066b4670fe07f32
patsonluk <patson@solarwinds.cloud>
2021-07-16 12:54:50 -0700

    Changed default execution context for actor system

--- airline-web/app/websocket/package.scala
+++ airline-web/app/websocket/package.scala
@@ -5,5 +5,6 @@ import akka.actor.ActorSystem
 import scala.concurrent.ExecutionContext
 package object websocket {
   implicit val actorSystem = ActorSystem("airline-websocket-actor-system")
-  implicit val ec: ExecutionContext = ExecutionContext.global
+  //implicit val ec: ExecutionContext = ExecutionContext.global
+  implicit val executionContext = actorSystem.dispatchers.lookup("my-pinned-dispatcher")
 }
