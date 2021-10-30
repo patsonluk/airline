fabddd4e9862726b5b3a20966d8816ae11e9da9f
patsonluk <patson.luk@gmail.com>
2021-10-29 23:47:12 -0700

    Fixed equals Method

--- airline-data/src/main/scala/com/patson/model/IdObject.scala
+++ airline-data/src/main/scala/com/patson/model/IdObject.scala
@@ -3,7 +3,7 @@ package com.patson.model
 trait IdObject {
    def id : Int
    override def equals(other: Any) = other match {
-      case that: Airport  => if (id != 0) id == that.id else super.equals(that)
+      case that: IdObject  => if (id != 0) id == that.id else super.equals(that)
       case _                        => false
    }
    override def hashCode: Int = if (id != 0) id else super.hashCode()
