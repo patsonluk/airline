dc3994ba6fa20216a1b50f59f74c06cf5df7811d
patsonluk <patson.luk@gmail.com>
2021-10-30 00:39:44 -0700

    Removed IdObject optimization

--- airline-data/src/main/scala/com/patson/model/IdObject.scala
+++ airline-data/src/main/scala/com/patson/model/IdObject.scala
@@ -2,10 +2,5 @@ package com.patson.model
 
 trait IdObject {
    def id : Int
-   override def equals(other: Any) = other match {
-      case that: IdObject  => if (id != 0) id == that.id else super.equals(that)
-      case _                        => false
-   }
-   override def hashCode: Int = if (id != 0) id else super.hashCode()
 }
 
