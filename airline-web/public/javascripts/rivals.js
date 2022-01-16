f592f8e935cf2077c108af81efba5ade78a31a2c
patsonluk <patson.luk@gmail.com>
2022-01-16 10:06:32 -0800

    Fixed script issue in rivals screen

--- airline-web/public/javascripts/rivals.js
+++ airline-web/public/javascripts/rivals.js
@@ -236,7 +236,7 @@ function updateRivalBasicsDetails(airlineId) {
 	$("#rivalsCanvas .airlineGrade").html(getGradeStarsImgs(rival.gradeValue))
 
 	$("#rivalsCanvas .alliance").data("link", "alliance")
-	populateNavigation$("#rivalsCanvas .alliance")
+	populateNavigation($("#rivalsCanvas .alliance"))
 	if (rival.allianceName) {
 		$("#rivalsCanvas .alliance").text(rival.allianceName)
 		$("#rivalsCanvas .alliance").addClass("clickable")
