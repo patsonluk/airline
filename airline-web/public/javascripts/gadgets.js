dd56b69b374aa9fb5cb0d556268a3d3a2268575e
patsonluk <patson.luk@gmail.com>
2022-10-16 15:54:07 -0700

    https://github.com/patsonluk/airline/issues/516

--- airline-web/public/javascripts/gadgets.js
+++ airline-web/public/javascripts/gadgets.js
@@ -702,14 +702,3 @@ function isTouchDevice() {
   }
 }
 
-function scrollIntoView(element, container) {
-  var containerTop = $(container).scrollTop();
-  var containerBottom = containerTop + $(container).height();
-  var elemTop = element.offsetTop;
-  var elemBottom = elemTop + $(element).height();
-  if (elemTop < containerTop) {
-    $(container).scrollTop(elemTop);
-  } else if (elemBottom > containerBottom) {
-    $(container).scrollTop(elemBottom - $(container).height());
-  }
-}
