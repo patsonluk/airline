9097528f321bc8b3b6aa4b3bc02b42b882a9f199
patsonluk <patson.luk@gmail.com>
2022-10-17 22:55:29 -0700

    https://github.com/patsonluk/airline/issues/516

--- airline-web/public/javascripts/plot-fusioncharts.js
+++ airline-web/public/javascripts/plot-fusioncharts.js
@@ -677,20 +677,19 @@ function plotRivalHistory(rivalHistory, container, cycleHoverFunc, chartOutFunc)
     var dataset = []
     var paletteColors = []
     $.each(dataByAirlineId, function(airlineId, data) {
+        var color
+        if (airlineColors[airlineId]) {
+            color = airlineColors[airlineId]
+        } else {
+            color = colorFromString(airlineNameByAirlineId[airlineId]) //find a random color but steady one
+        }
+
         if (airlineId == activeAirline.id) { //always put own airline first (bottom)
             dataset.unshift({ seriesName: airlineNameByAirlineId[airlineId], "data" : data})
-            if (airlineColors[airlineId]) {
-                paletteColors.unshift(airlineColors[airlineId])
-            } else {
-                paletteColors.unshift('#d3d3d3')
-            }
+            paletteColors.unshift(color)
         } else {
             dataset.push({ seriesName: airlineNameByAirlineId[airlineId], "data" : data})
-            if (airlineColors[airlineId]) {
-                paletteColors.push(airlineColors[airlineId])
-            } else {
-                paletteColors.push('#d3d3d3')
-            }
+            paletteColors.push(color)
         }
     })
 
