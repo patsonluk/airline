ec193f8bf4d9f0cc0a9e1b88825a798b00ba7c6c
patsonluk <patson.luk@gmail.com>
2022-12-24 17:44:04 -0800

    Added dynamic airline name check

--- airline-web/public/javascripts/signup.js
+++ airline-web/public/javascripts/signup.js
@@ -1,41 +1,25 @@
-//$( document ).ready(function() {
-//	$.ajax({
-//		type: 'GET',
-//		url: "signup/profiles",
-//	    contentType: 'application/json; charset=utf-8',
-//	    dataType: 'json',
-//	    success: function(profiles) {
-//	    	$.each(profiles, function(index, profile) {
-//	    		showProfile(profile)
-//	  		});
-//	    	//updateModelInfo($('#modelInfo'))
-//	    },
-//        error: function(jqXHR, textStatus, errorThrown) {
-//	            console.log(JSON.stringify(jqXHR));
-//	            console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
-//	    }
-//	});
-//})
-
-//function showProfile(profile) {
-//	var html = '<div style="float:left;" class="profile-section verticalGroup clickable" onclick="selectProfile(' + profile.id + ', this)"><h1>' + profile.title +'</h1><br/><span class="label">' + profile.description + '</span><br/><br/><ul>'
-//	$.each(profile.outlines, function(index, outline) {
-//		html += '<li>' + outline + '</li>'
-//	})
-//	html += '</ul></div>'
-//
-//	var profileDiv = $(html).appendTo('#profiles')
-//	if ($('#profileId').val() == profile.id) {
-//		selectProfile(profile.id, profileDiv)
-//	}
-//}
-//
-//function selectProfile(profileId, profileDiv) {
-//	$('#profileId').val(profileId)
-//	$(profileDiv).siblings("div").removeClass("selected")
-//	$(profileDiv).addClass("selected")
-//}
-
+$( document ).ready(function() {
+    $('input#airlineName').on('input', function() {
+        var airlineName = $(this).val()
+        $.ajax({
+    		type: 'GET',
+    		url: "signup/airline-name-check?airlineName=" + airlineName,
+    	    contentType: 'application/json; charset=utf-8',
+    	    dataType: 'json',
+    	    success: function(result) {
+    	    	if (result.ok) {
+                    $('.airlineName dd.error').text('')
+    	    	} else {
+    	    	    $('.airlineName dd.error').text('This airline name is not available')
+    	    	}
+    	    },
+            error: function(jqXHR, textStatus, errorThrown) {
+    	            console.log(JSON.stringify(jqXHR));
+    	            console.log("AJAX error: " + textStatus + ' : ' + errorThrown);
+    	    }
+    	});
+    })
+})
 
 function signup(form) {
 	grecaptcha.ready(function() {
