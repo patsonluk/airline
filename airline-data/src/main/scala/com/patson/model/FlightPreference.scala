21cef53a837e49f32d78b144d5cf2391bdcf0cd9
spencerm <spnzrr@gmail.com>
2023-06-12 01:06:51 -0400

    mend

--- airline-data/src/main/scala/com/patson/model/FlightPreference.scala
+++ airline-data/src/main/scala/com/patson/model/FlightPreference.scala
@@ -165,7 +165,7 @@ abstract class FlightPreference(homeAirport : Airport) {
   //flightDurationMultiplier => how does wait time and speed affect ratio, 0 = no effect, 0.1 = 10%
   val tripDurationAdjustRatio = (link : Transport, linkClass : LinkClass) => {
     //shorter duration flights care much more about flight frequency
-    val frequencyImportance = Math.max(1, 60 * 5 / linkduration) * (frequencySensitivity / 2).toDouble
+    val frequencyImportance = Math.max(1, 60 * 5 / link.duration) * (frequencySensitivity / 2).toDouble
     val frequencyRatioDelta = (frequencyThreshold - 7).toDouble / frequencyThreshold * frequencyImportance
 
     val flightDurationRatioDelta =
