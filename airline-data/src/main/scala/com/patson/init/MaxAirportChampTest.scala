fc15955b1da2522df649b7dbf1d7f77f56cb06bb
patsonluk <patson.luk@gmail.com>
2023-12-25 11:12:23 -0800

    Updated test script

--- airline-data/src/main/scala/com/patson/init/MaxAirportChampTest.scala
+++ airline-data/src/main/scala/com/patson/init/MaxAirportChampTest.scala
@@ -1,6 +1,7 @@
 package com.patson.init
 
 import com.mchange.v2.c3p0.ComboPooledDataSource
+import com.patson.AirlineSimulation.MAX_AIRPORT_CHAMPION_BOOST_ENTRIES
 import com.patson.data.Constants.{DATABASE_CONNECTION, DATABASE_PASSWORD, DATABASE_USER, DB_DRIVER}
 import com.patson.data.{AirlineSource, AirportSource, ChristmasSource, Meta}
 import com.patson.model.christmas.SantaClausInfo
@@ -12,10 +13,18 @@ import scala.util.Random
 
 object MaxAirportChampTest extends App {
   val allInfo = ChampionUtil.loadAirportChampionInfo()
-  val sorted = allInfo.groupBy(_.loyalist.airline).toList.map {
+  val sortedBefore = allInfo.groupBy(_.loyalist.airline).toList.map {
     case (airline, entries) => (airline, (entries.map(_.reputationBoost).sum, entries.length))
   }.sortBy(_._2._2).reverse
 
-  sorted.foreach(println)
+  println("BEFORE!!!!!!!!!!!!!!!!")
+  sortedBefore.foreach(println)
+
+  val sortedAfter = allInfo.groupBy(_.loyalist.airline).toList.map {
+    case (airline, entries) => (airline, (entries.map(_.reputationBoost).sorted.takeRight(MAX_AIRPORT_CHAMPION_BOOST_ENTRIES).sum, MAX_AIRPORT_CHAMPION_BOOST_ENTRIES))
+  }.sortBy(_._2._2).reverse
+  println("AFTER!!!!!!!!!!!!!!!!!")
+
+
 
 }
