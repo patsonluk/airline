139f8bd31ebd189257d4fd509e633fe9e5b3104e
Bohaska <73286691+Bohaska@users.noreply.github.com>
2024-04-06 21:28:22 +0800

    Clicking on airport name in research tab goes to airport page

--- airline-web/public/javascripts/search.js
+++ airline-web/public/javascripts/search.js
@@ -800,9 +800,11 @@ function researchFlight(fromAirportId, toAirportId) {
                 loadAirportImage(fromAirportId, $('#researchSearchResult img.fromAirport') )
                 loadAirportImage(toAirportId, $('#researchSearchResult img.toAirport'))
                 $("#researchSearchResult .fromAirportText").text(result.fromAirportText)
+		$("#researchSearchResult .fromAirportText")[0].setAttribute("onclick", `showAirportDetails(${fromAirportId})`)
                 $("#researchSearchResult .fromAirport .population").text(commaSeparateNumber(result.fromAirport.population))
                 $("#researchSearchResult .fromAirport .incomeLevel").text(result.fromAirport.incomeLevel)
                 $("#researchSearchResult .toAirportText").text(result.toAirportText)
+		$("#researchSearchResult .toAirportText")[0].setAttribute("onclick", `showAirportDetails(${toAirportId})`)
                 $("#researchSearchResult .toAirport .population").text(commaSeparateNumber(result.toAirport.population))
                 $("#researchSearchResult .toAirport .incomeLevel").text(result.toAirport.incomeLevel)
 
