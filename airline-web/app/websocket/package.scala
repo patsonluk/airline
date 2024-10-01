e6609b2472686738e19070b14867720d9c0d56f0
K2 <stratus_chalet_0w@icloud.com>
2024-09-30 18:10:47 -0700

    Web:Play 3.x migration

--- airline-web/app/websocket/package.scala
+++ airline-web/app/websocket/package.scala
@@ -3,6 +3,7 @@
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.dispatch.MessageDispatcher
 
+
 import scala.concurrent.ExecutionContext
 package object websocket {
   implicit val actorSystem : ActorSystem = ActorSystem("airline-websocket-actor-system")
